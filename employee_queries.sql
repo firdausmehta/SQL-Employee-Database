098e3c4d0b53476e907fbefb9a57ffd42235057f
Firdaus Mehta <firmehta97@gmail.com>
2021-07-09 16:29:48 +1000

    Update to employee_db

--- employee_queries.sql
+++ employee_queries.sql
@@ -4,3 +4,18 @@ FROM employees
 JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE
+hire_date <= '1986-01-01'
+AND hire_date >= '1986-12-31';
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
